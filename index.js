0e16fe740730d6e4f71bb1f97f11cccf02e0088a
Lucas Wanderley <lucaswanderley4@gmail.com>
2019-05-24 01:36:03 +0000

    finish module 01

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
-const express = require("express");
-const nunjucks = require("nunjucks");
+const express = require('express')
+const nunjucks = require('nunjucks')
 
-const app = express();
+const app = express()
 
-nunjucks.configure("views", {
+nunjucks.configure('views', {
   autoescape: true,
   express: app,
   watch: true
-});
+})
 
-app.set("view engine", "njk");
+app.use(express.urlencoded({ extended: false }))
+app.set('view engine', 'njk')
 
-app.get("/", (req, res) => {
-  return res.render("list", { name: "Lucas" });
-});
+const users = ['Lucas Sousa', 'Claudio Silva', 'Maria Francisca']
 
-app.listen(3000);
+app.get('/', (req, res) => {
+  return res.render('list', { users })
+})
+
+app.get('/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/create', (req, res) => {
+  users.push(req.body.user)
+
+  res.redirect('/')
+})
+
+app.listen(3000)
